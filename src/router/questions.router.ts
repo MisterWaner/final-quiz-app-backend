8d449e82c6d64fcf0ab1b7f7b2029a9234e5aa4d
Erwan Soulas <erwan.soulas@gmail.com>
2025-03-15 00:06:07 +0100

    17th commit: --delete type.ts because unused. --update return type for questionsrouter

--- src/router/questions.router.ts
+++ src/router/questions.router.ts
@@ -2,60 +2,60 @@ import { FastifyInstance } from 'fastify';
 import { MathQuestionService } from '../infrastructure/question/math/math.question.service';   
 import { GeographyCapitalsQuizController } from '../infrastructure/quiz/geography/geography.quiz.capitals.controller';
 import { MathQuizController } from '../infrastructure/quiz/math/math.quiz.controller';
-import { Question, MultipleChoiceQuestion, TrueFalseQuestion } from '../domain/Question';
+import { Quiz } from '../domain/Quiz';
 
 const mathQuestionService = new MathQuestionService();
 const mathQuizController = new MathQuizController(mathQuestionService);
 const geographyCapitalsController = new GeographyCapitalsQuizController();
 
 export async function questionsRouter(fastify: FastifyInstance) {
-    fastify.get<{ Reply: Question[] }>(
+    fastify.get<{ Reply: Quiz }>(
         '/math/addition',
         {},
         mathQuizController.getAddition
     );
-    fastify.get<{ Reply: Question[] }>(
+    fastify.get<{ Reply: Quiz }>(
         '/math/soustraction',
         {},
         mathQuizController.getSubstraction
     );
-    fastify.get<{ Reply: Question[] }>(
+    fastify.get<{ Reply: Quiz }>(
         '/math/multiplication',
         {},
         mathQuizController.getMultiplication
     );
-    fastify.get<{ Reply: Question[] }>(
+    fastify.get<{ Reply: Quiz }>(
         '/math/random-operations',
         {},
         mathQuizController.getRandomOperation
     );
 
-    fastify.get<{ Reply: MultipleChoiceQuestion[] }>(
+    fastify.get<{ Reply: Quiz }>(
         '/geography/european-capitals',
         {},
         geographyCapitalsController.getEuropeanCapitals
     );
-    fastify.get<{ Reply: MultipleChoiceQuestion[] }>(
+    fastify.get<{ Reply: Quiz }>(
         '/geography/african-capitals',
         {},
         geographyCapitalsController.getAfricanCapitals
     );
-    fastify.get<{ Reply: MultipleChoiceQuestion[] }>(
+    fastify.get<{ Reply: Quiz }>(
         '/geography/asian-capitals',
         {},
         geographyCapitalsController.getAsianCapitals
     );
-    fastify.get<{ Reply: MultipleChoiceQuestion[] }>(
+    fastify.get<{ Reply: Quiz }>(
         '/geography/american-capitals',
         {},
         geographyCapitalsController.getAmericanCapitals
     );
-    fastify.get<{ Reply: MultipleChoiceQuestion[] }>(
+    fastify.get<{ Reply: Quiz }>(
         '/geography/oceanic-capitals',
         {},
         geographyCapitalsController.getOceaniaCapitals
     );
-    fastify.get<{ Reply: MultipleChoiceQuestion[] }>(
+    fastify.get<{ Reply: Quiz }>(
         '/geography/random-capitals',
         {},
         geographyCapitalsController.getRandomCapitals
